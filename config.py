30a32c5f706aec19fc1de0a3239eb84248230dc4
cassildobez2-star <cassildobez2@gmail.com>
2026-02-17 17:07:51 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # URL do Railway / domínio com SSL
+PORT = int(os.getenv("PORT", 8443))
+CBZ_CACHE_DIR = os.getenv("CBZ_CACHE_DIR", "./cbz_cache")
