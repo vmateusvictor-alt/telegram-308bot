d91fcf7aeeec0779a8d2f3d99b977a33ea1f034f
sporofitabez-ux <sporofitabez@gmail.com>
2026-02-20 13:57:29 +0200

    Update cbz.py

--- utils/cbz.py
+++ utils/cbz.py
@@ -5,6 +5,7 @@ import asyncio
 
 os.makedirs("tmp", exist_ok=True)
 
+
 async def download_image(client, url):
     try:
         r = await client.get(url, timeout=60)
@@ -14,24 +15,36 @@ async def download_image(client, url):
         print(f"Erro ao baixar imagem: {e}")
         return None
 
-async def create_cbz(image_urls, manga_title, chapter_name):
-    safe_title = manga_title.replace("/", "").replace(" ", "_")
-    safe_chapter = str(chapter_name).replace("/", "").replace(" ", "_")
 
-    cbz_filename = f"{safe_title}_{safe_chapter}.cbz"
+async def create_volume_cbz(source, chapters, manga_title, volume_name):
+    safe_title = manga_title.replace("/", "").replace(" ", "_")
+    safe_volume = volume_name.replace("/", "").replace(" ", "_")
+
+    cbz_filename = f"{safe_title}_{safe_volume}.cbz"
     cbz_path = os.path.join("tmp", cbz_filename)
 
-    async with httpx.AsyncClient() as client:
-        tasks = [download_image(client, url) for url in image_urls]
-        images = await asyncio.gather(*tasks)
+    semaphore = asyncio.Semaphore(10)
 
-    images = [img for img in images if img]
+    async with httpx.AsyncClient(timeout=60) as client:
+        with zipfile.ZipFile(cbz_path, "w", compression=zipfile.ZIP_DEFLATED) as cbz:
+            page_counter = 1
 
-    if not images:
-        raise Exception("Nenhuma imagem foi baixada")
+            for chapter in chapters:
+                pages = await source.pages(chapter.get("url"))
 
-    with zipfile.ZipFile(cbz_path, "w") as cbz:
-        for i, img_bytes in enumerate(images):
-            cbz.writestr(f"{i+1}.jpg", img_bytes)
+                async def limited_download(url):
+                    async with semaphore:
+                        return await download_image(client, url)
+
+                tasks = [limited_download(url) for url in pages]
+                images = await asyncio.gather(*tasks)
+
+                for img in images:
+                    if img:
+                        cbz.writestr(f"{page_counter}.jpg", img)
+                        page_counter += 1
+
+    if not os.path.exists(cbz_path):
+        raise Exception("Erro ao criar volume")
 
     return cbz_path, cbz_filename
